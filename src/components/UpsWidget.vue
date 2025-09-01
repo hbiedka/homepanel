663fb5e368c543de5cdb1399df96e88f5945c2d0
Hubert Biedka <hbiedka.gmail.com>
2025-09-01 11:30:47 +0200

    Add UPS widget

---
+++ src/components/UpsWidget.vue
@@ -0,0 +1,91 @@
+<template>
+    <v-card>
+        <v-card-title class="d-flex align-center">
+            <v-icon class="mr-3" size="small">mdi-car-battery</v-icon>
+            <p style="font-size:smaller">
+                <slot></slot>
+            </p>
+            <v-spacer></v-spacer>
+            <!-- {{ upsBatterySoc }} % -->
+                <p style="border-radius: 5px; font-size:smaller;" :style="stateColor" class="px-2 py-0">
+                    {{ stateStr }}
+                </p>
+        </v-card-title>
+        <v-card-text class="mt-2">
+            <p class="d-flex mb-1">
+                SoC
+                <v-spacer></v-spacer>
+                <b>{{ batterySoc }} %</b>
+            </p>
+            <p class="d-flex mb-1">
+                Battery voltage
+                <v-spacer></v-spacer>
+                <b>{{ batteryVoltage }} V</b>
+            </p>
+            <p class="d-flex mb-1">
+                Input voltage
+                <v-spacer></v-spacer>
+                <b>{{ inVoltage }} V</b>
+            </p>
+            <p class="d-flex mb-1">
+                Output voltage
+                <v-spacer></v-spacer>
+                <b>{{ outVoltage }} V</b>
+            </p>
+            
+        </v-card-text>
+    </v-card> 
+</template>
+
+<script>
+
+import { useStore } from "@/stores/api"
+import { toValue } from "vue"
+
+export default {
+    setup() {
+        const store = useStore()
+        return { store }
+    },
+    props: {
+        name: String
+    },
+    computed: {
+        ups() {
+            const path = this.name.split('/')
+            let value = this.store?.data
+            for (const key of path) {
+                value = value?.[key]
+            }
+            return value
+        },
+        state() {
+            return this.ups?.ups?.status?.value || "?"
+        },
+        stateStr() {
+            const s = this.state
+            if (s === "OL") return "Online"
+            if (s === "OB") return "On battery"
+
+            return s
+        },
+        stateColor() {
+            return `background-color: ${this.state == "OL" ? "#1B5E20" : "#D32F2F"};`
+        },
+        batterySoc() {
+            return this.ups?.battery?.charge?.value || "?"
+        },
+        batteryVoltage() {
+            return this.ups?.battery?.voltage?.value || "?"
+        },
+        inVoltage() {
+            return this.ups?.input?.voltage?.value || "?"
+        },
+        outVoltage() {
+            return this.ups?.output?.voltage?.value || "?"
+        },
+
+    }
+}
+
+</script>
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import TemperatureWidget from './TemperatureWidget.vue'
 import OnOffWidget from './OnOffWidget.vue'
 import MultiButtonWidget from './MultiButtonWidget.vue'
+import UpsWidget from './UpsWidget.vue'
 
-export { TemperatureWidget, OnOffWidget, MultiButtonWidget }
+export { TemperatureWidget, OnOffWidget, MultiButtonWidget, UpsWidget }
